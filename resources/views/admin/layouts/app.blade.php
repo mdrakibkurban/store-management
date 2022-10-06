53cb67927e3ebc22c6d8a18a5dc2d9c4723aaead
Rakib Hasan <hasanrr092@gmail.com>
2022-10-06 23:22:41 +0600

    project setup

---
+++ resources/views/admin/layouts/app.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<!--
+This is a starter template page. Use this page to start your new project from
+scratch. This page gets rid of all links and provides the needed markup only.
+-->
+<html lang="en">
+<head>
+  @include('admin.layouts.head')
+</head>
+<body class="hold-transition sidebar-mini">
+<div class="wrapper">
+
+  <!-- Navbar -->
+  @include('admin.layouts.nav')
+
+
+  @include('admin.layouts.aside')
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0">DashBoard</h1>
+          </div><!-- /.col -->
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item active">Starter Page</li>
+            </ol>
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    <div class="content">
+          @yield('content')
+    </div>
+    <!-- /.content -->
+  </div>
+   
+  @include('admin.layouts.footer')
+     
+</div>
+<!-- ./wrapper -->
+
+@include('admin.layouts.scripts')
+</body>
+</html>
