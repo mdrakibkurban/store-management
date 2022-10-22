76e86240fa87eb0c074c40421ac49df161741c76
Rakib Hasan <hasanrr092@gmail.com>
2022-10-23 00:05:10 +0600

    add files

--- resources/views/admin/layouts/aside.blade.php
+++ resources/views/admin/layouts/aside.blade.php
@@ -6,7 +6,7 @@
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="info">
-          <a href="#" class="d-block">
+          <a href="javascript:void(0)" class="d-block">
             <i class="nav-icon fas fa-user "></i>
             {{ strtoUpper(auth()->user()->name ?? '') }}
           </a>
